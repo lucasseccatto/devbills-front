d2915b7897c025ac36e66759ea6f2f8079013c0b
lucasseccatto <lucasseccatto1@gmail.com>
2024-02-20 18:43:56 -0300

    Atualizacao da pagina Home, com adicao de novos componentes, estilos, funcionalidades, e melhorias na experiencia do usuario

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,133 @@
+import { InputMask } from '@react-input/mask';
 import { Button } from '../../components/button';
+import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
-import { Header } from './styles';
+import { Title } from '../../components/title';
+import {
+  Aside,
+  Balance,
+  ChartAction,
+  ChartContainer,
+  ChartContent,
+  Filters,
+  Header,
+  InputGroup,
+  Main,
+  SearchTransaction,
+  Section,
+} from './styles';
+import { ButtonIcon } from '../../components/button-icon';
+import { Card } from '../../components/card';
+import { Transaction } from '../../components/transaction';
 
 export function Home() {
   return (
-    <Header>
-      <Logo />
-      <div>
-        <Button>Nova transação</Button>
-        <Button>Nova categoria</Button>
-      </div>
-    </Header>
+    <>
+      <Header>
+        <Logo />
+        <div>
+          <Button>Nova transação</Button>
+          <Button>Nova categoria</Button>
+        </div>
+      </Header>
+      <Main>
+        <Section>
+          <Filters>
+            <Title title="Saldo" subtitle="Receitas e despesas no período" />
+            <InputGroup>
+              <InputMask
+                component={Input}
+                mask="dd/mm/aaaa"
+                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
+                variant="dark"
+                label="Início"
+                placeholder="dd/mm/aaaa"
+              />
+              <InputMask
+                component={Input}
+                mask="dd/mm/aaaa"
+                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
+                variant="dark"
+                label="Fim"
+                placeholder="dd/mm/aaaa"
+              />
+              <ButtonIcon />
+            </InputGroup>
+          </Filters>
+          <Balance>
+            <Card title="Saldo" amount={1000000} />
+            <Card title="Saldo" amount={1000000} variant="incomes" />
+            <Card title="Saldo" amount={1000000} variant="expenses" />
+          </Balance>
+          <ChartContainer>
+            <header>
+              <Title
+                title="Gastos"
+                subtitle="Despesas por categoria no período"
+              />
+            </header>
+            <ChartContent></ChartContent>
+          </ChartContainer>
+          <ChartContainer>
+            <header>
+              <Title
+                title="Evolução Financeira"
+                subtitle="Saldo, Receitas e Gastos no ano"
+              />
+
+              <ChartAction>
+                <InputMask
+                  component={Input}
+                  mask="dd/mm/aaaa"
+                  replacement={{ a: /\d/ }}
+                  variant="black"
+                  label="Ano"
+                  placeholder="aaaa"
+                />
+                <ButtonIcon />
+              </ChartAction>
+            </header>
+            <ChartContent></ChartContent>
+          </ChartContainer>
+        </Section>
+        <Aside>
+          <header>
+            <Title title="Transações" subtitle="Receitas e Gastos no período" />
+            <SearchTransaction>
+              <Input variant="black" placeholder="Procurar transação..." />
+              <ButtonIcon />
+            </SearchTransaction>
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#FF33bb' }}
+              title="Mercado"
+            />
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#FF33bb' }}
+              title="Mercado"
+            />
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#FF33bb' }}
+              title="Mercado"
+            />
+            <Transaction
+              id={1}
+              amount={20000}
+              date="09/09/2023"
+              category={{ title: 'Alimentação', color: '#FF33bb' }}
+              title="Mercado"
+            />
+          </header>
+        </Aside>
+      </Main>
+    </>
   );
 }
